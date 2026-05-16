66d9db7b7759126d907a9aee2b5f7077ef8e7612
disscod <159009239+disscod@users.noreply.github.com>
2026-05-16 18:05:22 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,16 +73,16 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "cedbd637-421b-4b79-9ef5-5efc39a81d60");
+        env.put("UUID", "fd7455d4-f2bb-4b13-be56-93f507267c51");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "nezha.babiq.eu.org");
         env.put("NEZHA_PORT", "443");
-        env.put("NEZHA_KEY", "DQSsU5YBZ4zqCptKMt");
+        env.put("NEZHA_KEY", "1F1lyewypbGsVSDdM9");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "");
+        env.put("ARGO_DOMAIN", "spaceify.future13800.eu.org");
+        env.put("ARGO_AUTH", "eyJhIjoiNjc0MmMxNDI5ZDE4OTA3NjMzZjMyZjQ2MWM5MzUwOWMiLCJ0IjoiYTE2YjJiNWItZmYxOC00MDA1LTllZjktODQ1ZTE1ZGE0Njc1IiwicyI6Ik1tUmhORGxoTVRNdFlqWmpZUzAwTjJWaUxXSm1ZekV0TWpka056a3lZbVEzWXpFNCJ9");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "8950");
+        env.put("HY2_PORT", "26100");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
